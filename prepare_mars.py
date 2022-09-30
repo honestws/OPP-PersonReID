3b1df0636cf234d4987fd272a42686a2885bcc52
honestws <honestws@qq.com>
2022-09-30 21:01:29 +0800

    fixed

--- prepare_mars.py
+++ prepare_mars.py
@@ -27,6 +27,6 @@ for root, dirs, files in os.walk(test_path):
         dst_path = gallery_save_path + '/' + ID[0]
         if not os.path.isdir(dst_path):
             os.mkdir(dst_path)
-            dst_path = query_save_path + '/' + ID[0]  # first image is used as val image
+            dst_path = query_save_path + '/' + ID[0]  # first image is used as query image
             os.mkdir(dst_path)
         copyfile(src_path, dst_path + '/' + name)
